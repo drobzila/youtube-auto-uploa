411d38349ccc61aaf38f5c9bcf49e6a2ab2ac5f8
drobliza <lesquatrefreresazri@gmail.com>
2025-07-17 06:43:47 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,3 +1,9 @@
 # script.py
 
-print("Hello, World!")
+log_file = 'log.txt'  # اسم الملف الذي سيتم إضافة السجلات فيه
+
+# كتابة النص في الملف
+with open(log_file, 'a') as f:  # فتح الملف في وضع الإضافة (append)
+    f.write("Hello, World!\n")  # كتابة "Hello, World!" في الملف
+
+print("Hello, World!")  # الطباعة في GitHub Actions لعرضها في السجلات
