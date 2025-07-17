cdeafd53b4a2760cebc0e67c2a3e1d541119c2c8
drobliza <lesquatrefreresazri@gmail.com>
2025-07-17 07:15:33 +0100

    Update script.py

--- script.py
+++ script.py
@@ -2,12 +2,9 @@ import time
 
 log_file = 'log.txt'
 
-# سيعمل السكربت فقط لمدة دقيقة واحدة ويدخل في حلقة يكتب فيها "Hello, World!" كل 5 ثوانٍ
-end_time = time.time() + 60  # تحديد الوقت لإنهاء السكربت بعد دقيقة واحدة
-
-while time.time() < end_time:
+# سيكتب السكربت 5 مرات في الدقيقة
+for _ in range(5):  # يكتب 5 مرات
     with open(log_file, 'a') as f:
         f.write("Hello, World!\n")
     print("Hello, World!")  # الطباعة في الـ console
-
-    time.sleep(5)  # الانتظار لمدة 5 ثوانٍ قبل الكتابة مرة أخرى
+    time.sleep(12)  # الانتظار لمدة 12 ثانية قبل الكتابة مرة أخرى
