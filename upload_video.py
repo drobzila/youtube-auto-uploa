054d8741076839f7fa7c741f0bc0525bf230c06f
drobliza <lesquatrefreresazri@gmail.com>
2025-07-19 15:32:56 +0100

    Update upload_video.py

--- upload_video.py
+++ upload_video.py
@@ -4,8 +4,8 @@ import time
 import datetime
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseDownload, MediaFileUpload
-from google.oauth2.credentials import Credentials
 from google.oauth2.service_account import Credentials
+from google.oauth2.service_account import Credentials as ServiceAccountCredentials
 
 # إعداد Google Drive API
 def get_drive_service():
